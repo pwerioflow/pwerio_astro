f35e39a0719e5f062254fa1a203a493f56373094
eric c nice <ericnice1@gmail.com>
2025-04-23 11:06:43 -0300

    7º Commit

--- src/navigation.ts
+++ src/navigation.ts
@@ -5,8 +5,8 @@ export const headerData = {
     {text: 'Sobre', href: '#features'},
     { text: 'Tech', href: '#tech' },
     { text: 'Agência', href: '#agency' },
-    { text: 'FAQ', href: '#faq' },
     {text: 'Contratar',href: '#hire',},
+    { text: 'FAQ', href: '#faq' },
   ],
   actions: [{ text: 'Entre em Contato', href: 'https://wa.me/5511950507175?text=Estava%20no%20site%20da%20PWER%20e%20me%20interessei%20pelos%20serviços%20prestados.%20Gostaria%20de%20mais%20detalhes.', target: '_blank' }],
 };
@@ -22,7 +22,6 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: `
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://pwer.com.br/"> PWER IO</a> · All rights reserved.
